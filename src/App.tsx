41fce7666703d0883ec78f589932a83eb95cd532
merveartut <merveartuttt@gmail.com>
2025-06-25 11:51:20 +0300

    redirect path changed

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,7 @@ import { SettingsPage } from "./pages/SettingsPage/SettingsPage";
 import { Tasks } from "./pages/DashboardPage/Tasks";
 import { Toaster } from "react-hot-toast";
 import { ProfilePage } from "./pages/ProfilePage/ProfilePage";
+import { JSX } from "react";
 
 function App() {
   return (
@@ -57,6 +58,16 @@ function AppContent() {
     userRole: currentUserRole,
   };
 
+  function PrivateRoute({ children }: { children: JSX.Element }) {
+    const token = localStorage.getItem("token");
+
+    if (token || currentUserRole === "GUEST") {
+      return children;
+    }
+
+    return <Navigate to="/login" />;
+  }
+
   return (
     <div className="flex h-screen">
       {!hideNavbarRoutes.includes(location.pathname) && (
@@ -75,14 +86,56 @@ function AppContent() {
       <div className="flex-1 overflow-y-auto md:pt-0 lg:pt-0 pt-16">
         <Routes>
           <Route path="/login" element={<LoginPage />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/profile/:id" element={<ProfilePage />} />
-          <Route path="/projectDetail/:id" element={<ProjectDetailPage />} />
-          <Route path="/taskDetail/:id" element={<TaskDetailPage />} />
-          <Route path="/settings" element={<SettingsPage />} />
-          <Route path="/tasks" element={<Tasks />} />
-          <Route path="/*" element={<Navigate to="/login" />} />{" "}
-          {/* Redirect unknown routes to login */}
+          <Route
+            path="/projects"
+            element={
+              <PrivateRoute>
+                <Projects />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/profile/:id"
+            element={
+              <PrivateRoute>
+                <ProfilePage />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/projectDetail/:id"
+            element={
+              <PrivateRoute>
+                <ProjectDetailPage />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/taskDetail/:id"
+            element={
+              <PrivateRoute>
+                <TaskDetailPage />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/settings"
+            element={
+              <PrivateRoute>
+                <SettingsPage />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/tasks"
+            element={
+              <PrivateRoute>
+                <Tasks />
+              </PrivateRoute>
+            }
+          />
+          <Route path="*" element={<Navigate to="/projects" />} />{" "}
+          {/* Default route */}
         </Routes>
       </div>
     </div>
